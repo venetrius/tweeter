ab323f66f3d9fa9bd5b12055b6aab72270713906
venetrius <gergelygjuhasz@gmail.com>
2019-05-14 15:13:03 -0600

    implement 'character counter'

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
   $('textarea').on("input", function(event){
     this.form.lastElementChild.innerText = 140 - this.value.length;
-    let color = 140 - this.value.length < 0 ? "red" : "";
-    $(this.form.lastElementChild).css("background-color", color);
+    if(140 - this.value.length < 0){
+      $(this.form.querySelector('output')).addClass("overflow");
+    }else{
+      $(this.form.querySelector('output')).removeClass("overflow");
+    }
   });
 });
