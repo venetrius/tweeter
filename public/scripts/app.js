898b68f20677f492942748d840419c6bfb8e770a
venetrius <gergelygjuhasz@gmail.com>
2019-05-14 21:20:59 -0600

    implement dynamic tweet rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,112 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ /*
+ Example data for testing
+ TODO remove (or not?)
+ */
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+const createTweetHeader = function(user){
+  let $header =
+    `<header>
+      <img src=${user.avatars.small}>
+      <span class="name"> ${user.name} </span>
+      <span class="signature"> ${user.handle} </span>
+    </header>`;
+    return $header;
+}
+
+const createTweetContent = function(content){
+  let $content =
+          `<div>
+            <span>${content.text}</span>
+          </div>`
+  return $content;
+}
+
+const createTweetFooter = function(timeStamp){
+  const diffTime = new Date().getTime() - timeStamp;
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const $footer =
+    `<footer> ${diffDays} days ago
+            <img src="./../images/arrows.png"/>
+            <img src="./../images/heart.png"/>
+            <img src="./../images/flag.png"/>
+    </footer>`
+    return $footer;
+}
+
+const createTweetElement = function(tweet){
+  let $header = createTweetHeader(tweet.user);
+  let $content = createTweetContent(tweet.content);
+  let $footer = createTweetFooter(tweet.created_at);
+  let $tweet = `<article>
+                  ${$header}
+                  ${$content}
+                  ${$footer}
+                </article>`;
+  return $tweet;
+
+ }
+
+$( document ).ready(function() {
+
+  function renderTweets(tweets) {
+    const tweetsContainer = $('#tweets-container');
+    for(let tweet of tweets){
+      tweetsContainer.append(createTweetElement(tweet));
+    };
+  }
+
+  renderTweets(tweetData);
+});
+
+
+
+
