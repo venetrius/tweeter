9bec3ef97b998545aaa689a009b969406cb4ae0a
venetrius <gergelygjuhasz@gmail.com>
2019-05-15 16:35:02 -0600

    fix bug caused by using lastchildElement

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('textarea').on("input", function(event){
-    this.form.lastElementChild.innerText = 140 - this.value.length;
+    $(this.form.querySelector('output')).text(140 - this.value.length);
     if(140 - this.value.length < 0){
       $(this.form.querySelector('output')).addClass("overflow");
     }else{
